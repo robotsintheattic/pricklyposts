4c62d81988fffb79cb6a3e26ace01deb088c9c14
robotsintheattic <katherine.r.johnson@gmail.com>
2017-05-21 13:39:09 -0600

    tweak to modules.js

--- server/routes/modules.js
+++ server/routes/modules.js
@@ -27,49 +27,49 @@ router.get('/modules/:id', function(req, res, next) {
     })
 })
 
-/* POST one module */
-router.post('/modules', function(req, res, next) {
-  knex('modules')
-    .returning(['id', 'type', 'title'])
-    .insert({
-      type: req.body.type
-    })
-    .then((modules) => {
-      res.send(modules)
-    })
-    .catch((error) => {
-      console.log(error)
-      next(error)
-    })
-})
-
-/* UPDATE one module */
-router.patch('/modules/:id', function(req, res, next) {
-  knex('modules')
-  .udpate({
-    type: req.body.type
-  })
-  .then((modules) => {
-    res.send(modules)
-  })
-  .catch((error) => {
-    console.log(error)
-    next(error)
-  })
-})
-
-/* DELETE one module */
-router.delete('modules/:id', function(req, res, next) {
-  knex('modules')
-  .where('id', req.params.id)
-  .del()
-  .then((modules) => {
-    res.send(modules)
-  })
-  .catch((error) => {
-    console.log(error)
-    next(error)
-  })
-})
+// /* POST one module */
+// router.post('/modules', function(req, res, next) {
+//   knex('modules')
+//     .returning(['id', 'type', 'title'])
+//     .insert({
+//       type: req.body.type
+//     })
+//     .then((modules) => {
+//       res.send(modules)
+//     })
+//     .catch((error) => {
+//       console.log(error)
+//       next(error)
+//     })
+// })
+//
+// /* UPDATE one module */
+// router.patch('/modules/:id', function(req, res, next) {
+//   knex('modules')
+//   .udpate({
+//     type: req.body.type
+//   })
+//   .then((modules) => {
+//     res.send(modules)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//     next(error)
+//   })
+// })
+//
+// /* DELETE one module */
+// router.delete('modules/:id', function(req, res, next) {
+//   knex('modules')
+//   .where('id', req.params.id)
+//   .del()
+//   .then((modules) => {
+//     res.send(modules)
+//   })
+//   .catch((error) => {
+//     console.log(error)
+//     next(error)
+//   })
+// })
 
 module.exports = router

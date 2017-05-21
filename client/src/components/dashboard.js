b3c1fb1702b2eb858503d219b1b50b9a41bffbe2
jenniferwiner <jeniwiner@gmail.com>
2017-05-21 16:26:07 -0600

    INSTAGRAM WORKS

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,14 +1,34 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import $ from 'jquery'
 
 class Dashboard extends Component {
   constructor(props) {
     super(props)
 
-    if (localStorage.token) {
-      console.log('token exists');
+    if (!localStorage.token) {
+      window.location.href = '/'
     }
   }
+
+  componentDidMount() {
+    let userData = ''
+    $.ajax({
+      method: 'get',
+      url: `https://api.instagram.com/v1/users/self/?access_token=${localStorage.token}`,
+      dataType: 'jsonp',
+      success: function(result) {
+        userData = result.data
+      },
+      error: function(err) {
+        console.log(err);
+      }
+    }).then(() => {
+
+      console.log(userData);
+    })
+  }
+
   render() {
       return (
           <div>

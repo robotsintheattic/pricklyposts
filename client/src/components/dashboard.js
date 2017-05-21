3a0107b472233e3468cc0ff97acab16befb6234f
jenniferwiner <jeniwiner@gmail.com>
2017-05-21 13:37:53 -0600

    beginning ajax on dashboard

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <h1>On the dashboard</h1>
-                <Link to='/'>Home</Link>
-            </div>
-        )
+  constructor(props) {
+    super(props)
+
+    if (localStorage.token) {
+      console.log('token exists');
     }
+  }
+  render() {
+      return (
+          <div>
+              <h1>On the dashboard</h1>
+              <Link to='/'>Home</Link>
+          </div>
+      )
+  }
 }
 
 export default Dashboard

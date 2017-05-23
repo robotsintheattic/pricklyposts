5b2f2d0d02eb646d8698199756cf59f1602728c5
robotsintheattic <katherine.r.johnson@gmail.com>
2017-05-23 11:06:13 -0600

    prepping to merge in Jen's changes to client/entry.js

--- client/src/components/modules/todo.js
+++ client/src/components/modules/todo.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
-import $ from 'jquery'
 import ContentEditable from 'react-contenteditable'
 
-const todo = React.createClass({
+const ToDoItems = React.createClass({
+  render: function () {
+
+  }
+})
+
+const ToDo = React.createClass({
+  getInitialState: funciton() {
+    return {
+      items: []
+    }
+  },
+
+  addItem: function(e) {
+    let itemArray = this.state.items
+
+    itemArray.push(
+      {
+        text: this._inputElement.value,
+        key: Date.now()
+      }
+    )
+
+    this.setState({
+      items: itemArray
+    })
+
+    this._inputElement.value = ""
+
+    e.preventDefault()
+  },
+
   render: function() {
     return (
       <div className="todoListMain">
         <div className="header">
-          <form>
-            <input placeholder="Make your list!">
+          <form onSubmit={this.addItem}>
+            <input ref={ (a) => this._inputElement = a } placeholder="Make your list!">
             </input>
             <button type="submit">add</button>
           </form>

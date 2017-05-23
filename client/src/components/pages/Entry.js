fbbc76140c7de257be3082ba7d7ef7904802d41e
robotsintheattic <katherine.r.johnson@gmail.com>
2017-05-23 12:41:47 -0600

    import text and quote modules to entry js

--- client/src/components/pages/Entry.js
+++ client/src/components/pages/Entry.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import Sidenav from './../sidenav'
 import  ToDo from './../modules/todo'
+import  Textfield from './../modules/textfield'
+import  BlockQuote from './../modules/blockquote'
 
 class Entry extends Component {
   render() {
@@ -11,6 +13,9 @@ class Entry extends Component {
               <h1>Entry</h1>
               <p><Link to='journals/'>Home</Link></p>
               <p><Link to='journal/'>Back to Journal</Link></p>
+              <ToDo />
+              <Textfield />
+              <BlockQuote />
           </div>
       )
   }

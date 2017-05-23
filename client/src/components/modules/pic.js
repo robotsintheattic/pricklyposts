03ca18d25d4c2879b301fb378d81112e720e9abc
jenniferwiner <jeniwiner@gmail.com>
2017-05-23 11:28:25 -0600

    delete console.logs

--- client/src/components/modules/pic.js
+++ client/src/components/modules/pic.js
@@ -9,21 +9,14 @@ class Pic extends Component {
     this.state = {
       recentPics: []
     }
-
-    // if (!localStorage.token) {
-    //   window.location.href = '/'
-    // }
   }
   componentDidMount() {
     let recentPics = ''
-    console.log('>>>')
     $.ajax({
       method: 'get',
       url: `https://api.instagram.com/v1/users/self/media/recent/?access_token=${localStorage.token}`,
       dataType: 'jsonp',
       success: function(result) {
-        console.log('result', result)
-        // this.recentPics = result.data
         return result.data
       },
       error: function(err) {
@@ -31,7 +24,6 @@ class Pic extends Component {
       }
     })
     .then((instaArray) => {
-      console.log('checking .then', instaArray)
       this.setState({
         recentPics: instaArray.data
       })
@@ -39,9 +31,7 @@ class Pic extends Component {
   }
     render() {
       let recent = this.state.recentPics
-      console.log('recent:', recent)
       let picsList = recent.map(function(picsDisplay) {
-        console.log('picsDisplay', picsDisplay.images.standard_resolution.url);
         return <img src={picsDisplay.images.standard_resolution.url} width="75%" className="insta-pics"/>
       })
       return (

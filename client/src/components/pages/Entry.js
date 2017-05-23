1fced0b152480271f300f02f04b5b56fe925c1c7
jenniferwiner <jeniwiner@gmail.com>
2017-05-23 13:04:24 -0600

    in middle

--- client/src/components/pages/Entry.js
+++ client/src/components/pages/Entry.js
@@ -7,7 +7,15 @@ class Entry extends Component {
   render() {
       return (
           <div>
-            <Sidenav />
+            <nav className="navbar navbar-default">
+              <div className="container-fluid">
+                <div className="navbar-header">
+                  <h1 className="Home-title"><img className="Home-img" src={localStorage.userPic}/> Welcome, {localStorage.userName}</h1>
+                  <Sidenav />
+                </div>
+              </div>
+            </nav>
+
               <h1>Entry</h1>
               <p><Link to='journals/'>Home</Link></p>
               <p><Link to='journal/'>Back to Journal</Link></p>

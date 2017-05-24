2b395c7ac7a50d4b284d5ec78fbf6d2be3c9e366
jenniferwiner <jeniwiner@gmail.com>
2017-05-24 15:04:16 -0600

    heroku

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json())
 app.use(cookieParser())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+  app.use(express.static('../client/build'))
 }
 app.use(express.static(path.join(__dirname, '../client')))
 app.use(express.static(path.join(__dirname, '/../', 'node_modules')))

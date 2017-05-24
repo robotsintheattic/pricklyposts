4d670fd5de63a3c485e6066bfff1f175b209bc97
robotsintheattic <katherine.r.johnson@gmail.com>
2017-05-24 10:05:40 -0600

    Add classes in app css and minor styling to mood module

--- client/src/components/modules/mood.js
+++ client/src/components/modules/mood.js
@@ -33,7 +33,7 @@ class Mood extends Component {
     return (
       <row>
         <div className="col-md-4 vcenter"><font size="5">Today I feel: </font></div>
-        <div className="col-md-8 vcenter"><font size="5"><ContentEditable
+        <div className="col-md-8 vcenter dashed-box"><font size="5"><ContentEditable
           html={this.state.text}
           disabled={false}
           onChange={this.handleChange}

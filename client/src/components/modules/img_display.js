0a8f309491f4433176d8150c426fbf40112e1a8d
jenniferwiner <jeniwiner@gmail.com>
2017-05-25 08:47:37 -0600

    pic play

---
+++ client/src/components/modules/img_display.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class ImgDisplay extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  componentDidMount() {
+    window.addEventListener('storage', function(e) {
+      document.querySelector
+    })
+  }
+  
+  render() {
+
+  }
+}
+
+export default ImgDisplay
--- client/src/components/modules/pic.js
+++ client/src/components/modules/pic.js
@@ -9,7 +9,17 @@ class Pic extends Component {
     this.state = {
       recentPics: []
     }
+    this.selectPic = this.selectPic.bind(this)
   }
+
+  selectPic(e) {
+    if (e.target.src) {
+      localStorage.setItem('img_src', e.target.src)
+    }
+  }
+
+
+
   componentDidMount() {
     let recentPics = ''
     $.ajax({
@@ -36,7 +46,7 @@ class Pic extends Component {
       })
       return (
         <div className='insta-collection'>
-          <div className="text-center">
+          <div className="text-center" onClick={this.selectPic}>
           {picsList}
           </div>
           <br/>

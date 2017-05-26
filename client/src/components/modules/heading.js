321f1369203cbe5e9914ed68114839fc1ff5b4c7
jenniferwiner <jeniwiner@gmail.com>
2017-05-25 21:11:15 -0600

    content editable fields fixed

--- client/src/components/modules/heading.js
+++ client/src/components/modules/heading.js
@@ -15,7 +15,7 @@ class Heading extends Component {
   }
 
   handleClick = (e) => {
-    let content = this.state.html.substring(5, this.state.html.length - 6)
+    let content = this.state.html.substring(4, this.state.html.length - 5)
     fetch(`/api/entries_modules/${this.props.entryModule.em_id}`, {
       method: 'PATCH',
       body: JSON.stringify({content: content}),
